3ad72fb4ae89fa23c1574cf1630a1e278fe331f8
Jehiah Czebotar <jehiah@gmail.com>
2012-11-01 00:04:36 -0400

    update s3 policy

--- contrib/generate_s3_upload_policy.py
+++ contrib/generate_s3_upload_policy.py
@@ -1,35 +1,51 @@
 import base64
-import hmac, sys
+import hmac
 import hashlib
+import tornado.options
 
 # NOTE: FOR SOME REASON THIS ISN'T WORKING
 # USE http://s3.amazonaws.com/doc/s3-example-code/post/post_sample.html
 
-l = '--local' in sys.argv
-params = {
-    'url':(l and "http://localhost:8081" or "http://www.gtfs-data-exchange.com"),
-    'bucket':(l and 'gtfs-devel' or 'gtfs')
-}
+def run(bucket, url, aws_secret):
+    # l = '--local' in sys.argv
+    # params = {
+    #     'url':(l and "http://localhost:8081" or "http://www.gtfs-data-exchange.com"),
+    #     'bucket':(l and 'gtfs-devel' or 'gtfs')
+    # }
 
-policy_document = """
-{"expiration": "2012-01-01T00:00:00Z",
-  "conditions": [ 
-    {"bucket": "%(bucket)s"}, 
-    ["starts-with", "$key", "queue/"],
-    {"acl": "private"},
-    {"success_action_redirect": "%(url)s/queue"},
-    ["eq", "$Content-Type", "application/zip"],
-    ["content-length-range", 0, 31457280],
-    ["starts-with","$x-amz-meta-user",""],
-    ["starts-with","$x-amz-meta-comments",""]
-    ]
-}
-""" % params
-print policy_document
-policy = base64.b64encode(policy_document.strip())
+    policy_document = """
+    {"expiration": "2013-01-01T00:00:00Z",
+      "conditions": [ 
+        {"bucket": "%(bucket)s"}, 
+        ["starts-with", "$key", "queue/"],
+        {"acl": "private"},
+        {"success_action_redirect": "%(url)s/queue"},
+        ["eq", "$Content-Type", "application/zip"],
+        ["content-length-range", 0, 31457280],
+        ["starts-with","$x-amz-meta-user",""],
+        ["starts-with","$x-amz-meta-comments",""]
+        ]
+    }
+    """ % dict(
+        bucket=bucket,
+        url=url,
+    )
+    print policy_document
+    policy = base64.b64encode(policy_document.replace('\n','').strip())
 
-signature = base64.b64encode(hmac.new(AWS_SECRET_ACCESS_KEY, policy, hashlib.sha1).digest())
+    signature = base64.b64encode(hmac.new(aws_secret.encode('utf-8'), policy, hashlib.sha1).digest())
     
-print policy
-print "-"*50
-print signature
+    print policy
+    print "-"*50
+    print signature
+
+if __name__ == "__main__":
+    tornado.options.define("bucket", default="gtfs", type=str)
+    tornado.options.define("url", default="http://www.gtfs-data-exchange.com", type=str)
+    tornado.options.define("aws_secret", type=str)
+    tornado.options.parse_command_line()
+    
+    o = tornado.options.options
+    run(o.bucket, o.url, o.aws_secret)
+
+    
